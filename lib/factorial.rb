7b81d189e26ecbc9591f737c3d22962316f2f0ea
Declan <declankolakowski@gmail.com>
2020-03-31 16:20:20 +0000

    added implicit subject handling and digit adding object

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -3,3 +3,9 @@ class Factorial
     (1..n).inject(:*)
   end
 end
+
+class Digit
+  def digit_sum(n)
+    n.to_s.split("").map { |v| v.to_i }.inject(:+)
+  end
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,10 +1,13 @@
 require 'factorial.rb'
 
 describe Factorial do
-  
-  let(:calculator) { Factorial.new }
-  
   it "finds the factorial of 5" do
-   expect(calculator.factorial_of(5)).to eq(120)
+   expect(subject.factorial_of(5)).to eq(120)
+  end
+end
+
+describe Digit do
+  it "sums digits of 12" do
+    expect(subject.digit_sum(12)).to eq(3)
   end
 end
