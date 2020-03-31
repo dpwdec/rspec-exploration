9234738d1c6c7f7abbb18e3a7f666b614e0d41c6
Declan <declankolakowski@gmail.com>
2020-03-31 16:04:38 +0000

    refactored test to use let and object persistence

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,8 +1,10 @@
 require 'factorial.rb'
 
 describe Factorial do
+  
+  let(:calculator) { Factorial.new }
+  
   it "finds the factorial of 5" do
-   calculator = Factorial.new
    expect(calculator.factorial_of(5)).to eq(120)
   end
 end
