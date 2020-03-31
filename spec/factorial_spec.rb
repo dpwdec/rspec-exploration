ab80d186fe9ed5dd602ea4f2ac2709e5a6ac8d39
Declan <declankolakowski@gmail.com>
2020-03-31 16:23:41 +0000

    expanded digit sum tests

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -10,4 +10,13 @@ describe Digit do
   it "sums digits of 12" do
     expect(subject.digit_sum(12)).to eq(3)
   end
+  it "sums digits of 326" do
+    expect(subject.digit_sum(326)).to eq(11)
+  end
+  it "sums digits of 0" do
+    expect(subject.digit_sum(0)).to eq(0)
+  end
+  it "sums digits of 20456" do
+    expect(subject.digit_sum(20456)).to eq(17)
+  end
 end
