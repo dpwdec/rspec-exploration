886ee9d2ffa0f33ffd433ea355872fd29f2b4e63
Declan <declankolakowski@gmail.com>
2020-04-01 12:29:56 +0000

    added different instance handling tests

--- lib/instancer.rb
+++ lib/instancer.rb
@@ -2,6 +2,7 @@ class Instancer
   
   def initialize
     @history = []
+    @o_array = [5]
   end
   
   def add_var(n_var)
@@ -10,6 +11,14 @@ class Instancer
   
   def history
     @history
+  end
+  
+  def o_array
+    @o_array
+  end
+  
+  def covert_to_hash
+    @o_array = {number: 5}
   end
   
 end
--- spec/instancer_spec.rb
+++ spec/instancer_spec.rb
@@ -1,18 +1,44 @@
 require 'instancer'
 
 describe Instancer do
-  before do
-    @u_instance = Instancer.new
-    @instace_var = 12
-    @u_instance.add_var(12)
+  context "var to 12" do
+    before do
+      @u_instance = Instancer.new
+      @instace_var = 12
+      @u_instance.add_var(12)
+    end
+    
+    it "it has record of var" do
+      expect(@u_instance.history).to include(@instace_var)
+    end
+    
+    it "includes added object" do
+      expect{ @u_instance.add_var(10) }.to change(@u_instance, :history).to include(10)
+    end
+    
+    it "converts array to Hash" do
+      expect{ @u_instance.covert_to_hash }.to change(@u_instance, :o_array).to be_a_kind_of(Hash)
+    end
   end
   
-  it "it has record of var" do
-    expect(@u_instance.history).to include(@instace_var)
-  end
-  
-  it "includes added object" do
-    expect{ @u_instance.add_var(10) }.to change(@u_instance, :history).to include(10)
+  context "var to 22" do
+    before do
+      @u_instance = Instancer.new
+      @instace_var = 22
+      @u_instance.add_var(22)
+    end
+    
+    it "it has record of var" do
+      expect(@u_instance.history).to include(@instace_var)
+    end
+    
+    it "includes added object" do
+      expect{ @u_instance.add_var(12) }.to change(@u_instance, :history).to include(12)
+    end
+    
+    it "converts array to Hash" do
+      expect{ @u_instance.covert_to_hash }.to change(@u_instance, :o_array).to be_a_kind_of(Hash)
+    end
   end
   
 end
