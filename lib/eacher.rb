aacea1a958b46093c0db3986e4b42ca9212d2fca
Declan <declankolakowski@gmail.com>
2020-04-01 09:17:12 +0000

    added eacher methods

--- lib/eacher.rb
+++ lib/eacher.rb
@@ -1,6 +1,20 @@
 class Eacher
   @@amount = 0
   
+  def initialize
+    puts "creating"
+    @mult = 5
+  end
+  
+  def setup
+    puts "set up"
+    @mult=10
+  end
+  
+  def mult_to(n)
+    return n*@mult
+  end
+  
   def self.amount=(number)
     @@amount=number
   end
--- spec/eacher_spec.rb
+++ spec/eacher_spec.rb
@@ -1,13 +1,23 @@
 require 'eacher.rb'
 
 describe Eacher do
-  before(:each) { Eacher.amount = 10 }
-  
-  it 'returns 15 when adding 5' do
-    expect(subject.add_to(5)).to eq(15)
+  let!(:n_each) { Eacher.new }
+  context 'when adding' do
+    before(:each) { Eacher.amount = 10 }
+    
+    it 'returns 15 when adding 5' do
+      expect(n_each.add_to(5)).to eq(15)
+    end
+    
+    it 'returns 17 when adding 7' do
+      expect(n_each.add_to(7)).to eq(17)
+    end
   end
-  
-  it 'returns 17 when adding 7' do
-    expect(subject.add_to(7)).to eq(17)
+  context 'when instantiated' do
+    before(:each) { n_each.setup }
+    
+    it 'return 20 when multiplying set up by 2' do
+      expect(n_each.mult_to(2)).to eq(20)
+    end
   end
 end
