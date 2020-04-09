387cf64d2d8671c0ca830c2e1e5e07b4951c1ec9
Declan <declankolakowski@gmail.com>
2020-04-09 12:40:51 +0000

    added verifying double expectations, arguments and required keyword argument tests

--- lib/instanced.rb
+++ lib/instanced.rb
@@ -2,4 +2,12 @@ class Inst
   def panic
     "freaking out"
   end
+  
+  def fold(n)
+    puts "folded at #{n}"
+  end
+  
+  def check(foo:)
+    puts "hello"
+  end
 end
--- spec/instanced_spec.rb
+++ spec/instanced_spec.rb
@@ -17,8 +17,46 @@ describe Inst, "#panic" do
     puts inst.panic
   end
   
-  it "fails to double a fake method" do
+  xit "fails to double a fake method" do
     inst = instance_double(Inst)
     allow(inst).to receive(:desperate) { "Still surviving" }
+  end
+  
+  it "receives a method" do
+    inst = instance_double("Inst", :panic => "Walang chill")
+    expect(inst).to receive(:panic)
+    inst.panic
+  end
+  
+  it "checks inputs" do
+    inst = instance_double(Inst, :fold => "folded at 5")
+    inst.fold(5)
+  end
+  
+  xit "fails wrong input" do
+    inst = instance_double(Inst, :fold => "folded at 5")
+    inst.fold
+  end
+  
+  it "checks rquire keywords" do
+    inst = instance_double(Inst, :check => "checked")
+    inst.check(foo: 7)
+  end
+  
+  xit "fails requirement" do
+    inst = instance_double(Inst, :check => "checked")
+    inst.check(7)
+  end
+  
+  it "expects" do
+    inst = instance_double(Inst)
+    expect(inst).to receive(:panic)
+    inst.panic
+  end
+  
+  it "does not expect" do
+    inst = instance_double(Inst)
+    expect(inst).to receive(:desperate)
+    inst.desperate
   end
 end
